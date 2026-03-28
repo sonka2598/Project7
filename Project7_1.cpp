9e2e5aa3cd5f6198c0417aa22acc7719f27b0a81
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-03-29 01:03:20 +0600

    Add files via upload

---
+++ Project7_1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a;
+	std::cout << "int: " << &a << "  ";
+	std::cout << sizeof(a) << "\n";
+
+	short b;
+	std::cout << "short: " << &b << "  ";
+	std::cout << sizeof(b) << "\n";
+
+	long c;
+	std::cout << "long: " << &c << "  ";
+	std::cout << sizeof(c) << "\n";
+
+	long long d;
+	std::cout << "long long: " << &d << "  ";
+	std::cout << sizeof(d) << "\n";
+
+	float e;
+	std::cout << "float: " << &e << "  ";
+	std::cout << sizeof(e) << "\n";
+
+	double f;
+	std::cout << "double: " << &f << "  ";
+	std::cout << sizeof(f) << "\n";
+
+	long double g;
+	std::cout << "long double: " << &g << "  ";
+	std::cout << sizeof(g) << "\n";
+
+	bool h;
+	std::cout << "bool: " << &h << "  ";
+	std::cout << sizeof(h) << "\n";
+
+	return 0;
+}
+
---
+++ Project7_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+void swap(int& a, int& b)
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return 0;
+}
